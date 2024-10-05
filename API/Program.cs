be175954bdd1182c14ea1d14ea7119bdcaf7b9a9
Omega3D <shachabens@gmail.com>
2024-10-05 16:48:43 +0300

    +client

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,20 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("https://localhost:4200")
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
+                      });
+});
 
 
 builder.Services.AddControllers();
@@ -16,6 +29,7 @@ builder.Services.AddEndpointsApiExplorer();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
