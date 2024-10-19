7c20d8ef742b83aeca29bac1b22d1023d2168633
Omega3D <shachabens@gmail.com>
2024-10-19 17:56:28 +0300

    auth

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css'
+})
+export class NavComponent implements OnInit{
+  model: any = {};
+  
+
+
+
+  ngOnInit(): void {
+    
+  }
+
+  constructor(protected accountService: AccountService){
+
+  }
+
+  login(){
+    this.accountService.login(this.model).subscribe(
+      {
+        next: response => {
+          console.log(response);
+        },
+        error: error => console.log(error)
+      }
+    )
+  }
+
+  logout(){
+    this.accountService.logout();
+  }
+
+}
